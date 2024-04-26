28b3dc9bca398929a0f9e5b6f52141bc2caa5fed
Cedric Legara <legara.cedric@gmail.com>
2024-04-26 21:26:02 +0800

    chore: update to be compatible with SDV 1.6 and SMAPI 4.0

--- QuickResponses/ModConfig.cs
+++ QuickResponses/ModConfig.cs
@@ -1,19 +1,14 @@
 ﻿using Microsoft.Xna.Framework;
-using Netcode;
 using StardewModdingAPI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QuickResponses
 {
     public class ModConfig
     {
         public bool EnableMod { get; set; } = true;
-        public SButton SelectFirstResponseKey { get; set; } = SButton.E;
+        public SButton SelectFirstResponseKey { get; set; } = SButton.G;
         public bool ShowNumbers { get; set; } = true;
+        public float NumberScale { get; set; } = 0.69f;
         public Color NumberColor { get; set; } = Color.Black;
     }
 }
--- QuickResponses/ModEntry.cs
+++ QuickResponses/ModEntry.cs
@@ -4,14 +4,11 @@ using StardewModdingAPI;
 using StardewValley;
 using StardewValley.BellsAndWhistles;
 using StardewValley.Menus;
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 
 namespace QuickResponses
 {
-    public class ModEntry : Mod 
+    public class ModEntry : Mod
     {
         public static ModEntry context;
 
@@ -28,27 +25,55 @@ namespace QuickResponses
 
             Helper.Events.Input.ButtonPressed += Input_ButtonPressed;
             Helper.Events.Display.RenderedActiveMenu += Display_RenderedActiveMenu;
+        }
 
+        /// <summary> Sets the value of a specified field, if it exists, from Helper.Reflection into the `storage` variable.
+        /// Assures `required` field is set to false, to be able to null-check.
+        /// Returns bool for success.</summary>
+        /// Made under the assumption that GetField method would return null. (Read as: I'm dumbo)
+        private bool RetrieveFieldValue<T>(object obj, string name, ref T storage)
+        {
+            IReflectedField<T> field = Helper.Reflection.GetField<T>(obj, name, false);
+            bool valid = field != null;
+            if (valid)
+                storage = field.GetValue();
+            return valid;
         }
 
         private void Display_RenderedActiveMenu(object sender, StardewModdingAPI.Events.RenderedActiveMenuEventArgs e)
         {
-            if (!Config.ShowNumbers || Game1.activeClickableMenu == null || !(Game1.activeClickableMenu is DialogueBox) || !Helper.Reflection.GetField<bool>(Game1.activeClickableMenu, "isQuestion").GetValue())
+            if (!Config.ShowNumbers || Game1.activeClickableMenu == null || !(Game1.activeClickableMenu is DialogueBox))
+                return;
+            bool isQuestion = true;
+            if (!RetrieveFieldValue(Game1.activeClickableMenu, "isQuestion", ref isQuestion))
+                return;
+            if (!isQuestion)
                 return;
 
             DialogueBox db = Game1.activeClickableMenu as DialogueBox;
-            if (Helper.Reflection.GetField<int>(db, "characterIndexInDialogue").GetValue() < db.getCurrentString().Length - 1 || Helper.Reflection.GetField<bool>(db, "transitioning").GetValue())
+            int charIdxInDialogue = 0;
+            bool isTransitioning = true;
+            if (!RetrieveFieldValue(db, "characterIndexInDialogue", ref charIdxInDialogue) ||
+                !RetrieveFieldValue(db, "transitioning", ref isTransitioning))
+                return;
+            if (charIdxInDialogue < db.getCurrentString().Length - 1 || isTransitioning)
                 return;
 
-            int x = Helper.Reflection.GetField<int>(Game1.activeClickableMenu, "x").GetValue();
-            int y = Helper.Reflection.GetField<int>(Game1.activeClickableMenu, "y").GetValue();
-            int heightForQuestions = Helper.Reflection.GetField<int>(Game1.activeClickableMenu, "heightForQuestions").GetValue();
-            List<Response> responses = Helper.Reflection.GetField<List<Response>>(Game1.activeClickableMenu, "responses").GetValue();
-            int count = responses.Count;
+            int x = 0;
+            int y = 0;
+            int heightForQuestions = 0;
+            Response[] responses = {};
+            if (!RetrieveFieldValue(Game1.activeClickableMenu, "x", ref x) ||
+                !RetrieveFieldValue(Game1.activeClickableMenu, "y", ref y) ||
+                !RetrieveFieldValue(Game1.activeClickableMenu, "heightForQuestions", ref heightForQuestions) ||
+                !RetrieveFieldValue(Game1.activeClickableMenu, "responses", ref responses))
+                return;
+
+            int count = responses.Length;
             int responseY = y - (heightForQuestions - Game1.activeClickableMenu.height) + SpriteText.getHeightOfString((Game1.activeClickableMenu as DialogueBox).getCurrentString(), Game1.activeClickableMenu.width - 16) + 44;
             for (int i = 0; i < count; i++)
             {
-                e.SpriteBatch.DrawString(Game1.dialogueFont, $"{i + 1}", new Vector2(x, responseY), Config.NumberColor, 0, Vector2.Zero, 0.4f, SpriteEffects.None, 0.86f);
+                e.SpriteBatch.DrawString(Game1.dialogueFont, $"{i + 1}", new Vector2(x  - 0.69f, responseY - 6.9f), Config.NumberColor, 0, Vector2.Zero, Config.NumberScale, SpriteEffects.None, 0.86f);
                 responseY += SpriteText.getHeightOfString(responses[i].responseText, Game1.activeClickableMenu.width) + 16;
             }
 
@@ -56,22 +81,38 @@ namespace QuickResponses
 
         private void Input_ButtonPressed(object sender, StardewModdingAPI.Events.ButtonPressedEventArgs e)
         {
-            if (Game1.activeClickableMenu == null || !(Game1.activeClickableMenu is DialogueBox) || !Helper.Reflection.GetField<bool>(Game1.activeClickableMenu, "isQuestion").GetValue())
+            if (Game1.activeClickableMenu == null || !(Game1.activeClickableMenu is DialogueBox))
                 return;
-            if (e.Button == Config.SelectFirstResponseKey && Helper.Reflection.GetField<List<Response>>(Game1.activeClickableMenu, "responses").GetValue().Count == 2)
+            bool isQuestion = true;
+            if (!RetrieveFieldValue(Game1.activeClickableMenu, "isQuestion", ref isQuestion))
+                return;
+            if (!isQuestion)
+                return;
+
+            Response[] responses = {};
+            if (!RetrieveFieldValue(Game1.activeClickableMenu, "responses", ref responses))
+                return;
+
+            List<SButton> sbs = new List<SButton> { SButton.D1, SButton.D2, SButton.D3, SButton.D4, SButton.D5, SButton.D6, SButton.D7, SButton.D8, SButton.D9, SButton.D0 };
+            if (sbs.Contains(e.Button) && sbs.IndexOf(e.Button) < responses.Length)
             {
-                Monitor.Log("Pressed selectFirstResponse button key on question dialogue");
-                Helper.Reflection.GetField<int>(Game1.activeClickableMenu, "selectedResponse").SetValue(0);
+                Monitor.Log($"Pressed {e.Button} key on question dialogue");
+                IReflectedField<int> selectedResponseField = Helper.Reflection.GetField<int>(Game1.activeClickableMenu, "selectedResponse", false);
+                if (selectedResponseField == null)
+                    return;
+                selectedResponseField.SetValue(sbs.IndexOf(e.Button));
                 Game1.activeClickableMenu.receiveLeftClick(0, 0, true);
                 Helper.Input.Suppress(e.Button);
                 return;
             }
 
-            List<SButton> sbs = new List<SButton> { SButton.D1, SButton.D2, SButton.D3, SButton.D4, SButton.D5, SButton.D6, SButton.D7, SButton.D8, SButton.D9, SButton.D0 };
-            if (sbs.Contains(e.Button) && sbs.IndexOf(e.Button) < Helper.Reflection.GetField<List<Response>>(Game1.activeClickableMenu, "responses").GetValue().Count)
+            if (e.Button == Config.SelectFirstResponseKey && responses.Length == 2)
             {
-                Monitor.Log($"Pressed {e.Button} key on question dialogue");
-                Helper.Reflection.GetField<int>(Game1.activeClickableMenu, "selectedResponse").SetValue(sbs.IndexOf(e.Button));
+                Monitor.Log($"Pressed {Config.SelectFirstResponseKey} button key on question dialogue");
+                IReflectedField<int> selectedResponseField = Helper.Reflection.GetField<int>(Game1.activeClickableMenu, "selectedResponse", false);
+                if (selectedResponseField == null)
+                    return;
+                selectedResponseField.SetValue(0);
                 Game1.activeClickableMenu.receiveLeftClick(0, 0, true);
                 Helper.Input.Suppress(e.Button);
                 return;
